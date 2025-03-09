600333620a77e69084a58661f7e8d9da84b86dc7
Lividede <modeyolivia@gmail.com>
2025-03-09 01:25:12 +0000

    Last change

--- git.py
+++ git.py
@@ -1,3 +1,5 @@
 print("I'm learning git")
 print("I was supposed to do this on Monday")
 print("I'm doing it on Saturday")
+print("I think I'm getting there")
+print("apparently I'm still not there :(")
